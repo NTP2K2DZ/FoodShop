99b2da585f54e95c50a0a145e85855263eeaf148
NTP2K2DZ <ndbestyasuo@gmail.comm>
2023-12-25 19:03:21 +0700

    Update all

---
+++ components/footer.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:ndbestyasuo@gmail.com.com">ndbestyasuo@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>07:00am to 22:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Ha Noi</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:0333948660">0333948660</a>
+      </div>
+
+   </section>
+
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
